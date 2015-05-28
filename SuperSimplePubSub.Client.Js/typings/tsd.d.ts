69d481a3862029dd37aa002c87cae74f9ad81045
Fabian Raetz <fabian.raetz@gmail.com>
2015-05-28 23:08:31 +0200

    add es6.d.ts for real

---
+++ SuperSimplePubSub.Client.Js/typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="signalr/signalr.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="chai/chai.d.ts" />
+/// <reference path="es6.d.ts"/>
